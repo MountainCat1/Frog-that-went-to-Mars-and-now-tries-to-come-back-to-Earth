79c4b3dd47dd0731e7b52de3200b15b295927f72
tbanas <Tomasz.Banas@finture.com>
2023-10-19 14:26:56 +0200

    added zenject

---
+++ WseiGamejam/Assets/Scripts/UI/PlayerDisplayUI.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDisplayUI : MonoBehaviour
+{
+    [SerializeField] private GameObject disconnectedDisplay;
+    [SerializeField] private GameObject connectedDisplay;
+    
+    [SerializeField] private Image inputDisplay;
+    
+}
