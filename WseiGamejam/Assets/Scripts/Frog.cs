9edaae2139523080046c1e6c5951847a9b93a704
tbanas <Tomasz.Banas@finture.com>
2023-10-20 01:13:42 +0200

    added frog dying also making frogs gay

--- WseiGamejam/Assets/Scripts/Frog.cs
+++ WseiGamejam/Assets/Scripts/Frog.cs
@@ -8,22 +8,22 @@ public class Frog : MonoBehaviour
     public static Frog Singleton { private set; get; }
 
     public static event Action FrogMoved;
+    public static event Action FromRemoved;
     
     private PlayerManager _playerManager;
+    private Player _runner;
 
     [SerializeField] float jumpLength = 1f;
 
     [SerializeField] LayerMask layerMask;
 
-    [HideInInspector]
-    public FrogSpawner frogSpawner;
-
     public void Awake()
     {
-        frogSpawner = FindAnyObjectByType<FrogSpawner>();
-        
-        if(Singleton)
-            Debug.Log("Singleton pooped their pants!");
+        if (Singleton)
+        {
+            Destroy(Singleton);
+            Singleton = this;
+        }
 
         Singleton = this;
     }
@@ -31,7 +31,14 @@ public class Frog : MonoBehaviour
     void Start()
     {
         _playerManager = FindObjectOfType<PlayerManager>();
-        _playerManager.RunnerPlayerInput.PlayerMoved += OnPlayerMoved;
+        _runner = _playerManager.Runner;
+        _runner.RunnerPlayerInput.PlayerMoved += OnPlayerMoved;
+    }
+
+
+    private void OnDestroy()
+    {
+        _runner.RunnerPlayerInput.PlayerMoved -= OnPlayerMoved;
     }
 
     private void Update()
@@ -72,14 +79,21 @@ public class Frog : MonoBehaviour
     {
         Debug.Log("Frog taking damage!");
 
-        Vector2 newStartPoint = Vector2.zero;
+        // Vector2 newStartPoint = Vector2.zero;
+        //
+        // if (frogSpawner != null)
+        // {
+        //     newStartPoint = frogSpawner.GetSpawnPoint();
+        // }
+        //
+        // transform.position = newStartPoint;
 
-        if (frogSpawner != null)
-        {
-            newStartPoint = frogSpawner.GetSpawnPoint();
-        }
+        Kill();
+    }
 
-        transform.position = newStartPoint;
+    private void Kill()
+    {
+        Remove();
     }
 
     void OnDrawGizmosSelected()
@@ -90,6 +104,7 @@ public class Frog : MonoBehaviour
 
     public void Remove()
     {
-        throw new NotImplementedException();
+        Destroy(gameObject);
+        FromRemoved?.Invoke();
     }
 }
