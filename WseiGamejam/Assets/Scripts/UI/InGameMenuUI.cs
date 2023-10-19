ce0edc3bb035e94cc851f438083794118e1561c4
tbanas <Tomasz.Banas@finture.com>
2023-10-19 21:46:16 +0200

    work on ingame menu

---
+++ WseiGamejam/Assets/Scripts/UI/InGameMenuUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InGameMenuUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI frogKilledDisplay;
+    [SerializeField] private TextMeshProUGUI frogEscapedDisplay;
+    [SerializeField] private TextMeshProUGUI timeDisplay;
+}
